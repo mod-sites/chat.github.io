38106ebd2b82b9943a739d576352a89a2c2e2ac6
modestkid <128833717+modestkid@users.noreply.github.com>
2023-11-22 17:58:39 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static('public'));
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, function () {
+    console.log(`listening on *:${port}`);
+});
