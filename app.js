1fdd48bf6839f307f31a9fbb4d99442313e8bf0a
modestkid <128833717+modestkid@users.noreply.github.com>
2023-11-22 18:03:36 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
     var socket = io();
 
     socket.on('chat message', function (msg) {
-        var messages = document.getElementById('messages');
-        var li = document.createElement('li');
-        li.textContent = msg;
-        messages.appendChild(li);
+        displayMessage(msg);
     });
 
     window.sendMessage = function () {
         var messageInput = document.getElementById('messageInput');
         var message = messageInput.value;
+        displayMessage(message);  // Display the sent message on the sender's side
         socket.emit('chat message', message);
         messageInput.value = '';
     };
+
+    function displayMessage(msg) {
+        var messages = document.getElementById('messages');
+        var li = document.createElement('li');
+        li.textContent = msg;
+        messages.appendChild(li);
+    }
 });
